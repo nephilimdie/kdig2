4c8d7572f73eaef8558df939975fe6177646248f
stefano.bassetto.nep@gmail.com <stefano.bassetto.nep@gmail.com>
2013-07-12 11:39:10 +0200

    update

---
+++ src/Kdig/ArchaeologicalBundle/Form/PresampleType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kdig\ArchaeologicalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PresampleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('remarks')
+            ->add('created')
+            ->add('updated')
+            ->add('isActive')
+            ->add('isPublic')
+            ->add('isDelete')
+            ->add('media')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Kdig\ArchaeologicalBundle\Entity\Presample'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'kdig_archaeologicalbundle_presampletype';
+    }
+}
