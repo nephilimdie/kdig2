89ba8a3d061b02b8a568894983a4f56f7eec5a17
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-12 11:52:40 +0200

    update

--- src/Kdig/ArchaeologicalBundle/Listener/DoctrineExtensionListener.php
+++ src/Kdig/ArchaeologicalBundle/Listener/DoctrineExtensionListener.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kdig\ArchaeoloicalBundle\Listener;
+namespace Kdig\ArchaeologicalBundle\Listener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
