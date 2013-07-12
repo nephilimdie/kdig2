cb55af07895352fbbedbb217bb3be415b744f51f
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-12 11:21:28 +0200

    update

--- src/Kdig/UserBundle/Entity/Ressource.php
+++ src/Kdig/UserBundle/Entity/Ressource.php
@@ -15,6 +15,7 @@ namespace Kdig\UserBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
+use Gedmo\Mapping\Annotation as Gedmo;
 /**
  * Kdig\UserBundle\Entity\Ressource
  * 
