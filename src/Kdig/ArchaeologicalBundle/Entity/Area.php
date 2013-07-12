9ca05c3d367c9790a35749bf791627103068a084
stefano.bassetto.nep@gmail.com <stefano.bassetto.nep@gmail.com>
2013-07-12 11:26:20 +0200

    update

--- src/Kdig/ArchaeologicalBundle/Entity/Area.php
+++ src/Kdig/ArchaeologicalBundle/Entity/Area.php
@@ -138,4 +138,432 @@ class Area {
     {
         return $this->getName();
     }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->uss = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->media = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->groups = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->userprofilearea = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Area
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set remarks
+     *
+     * @param string $remarks
+     * @return Area
+     */
+    public function setRemarks($remarks)
+    {
+        $this->remarks = $remarks;
+    
+        return $this;
+    }
+
+    /**
+     * Get remarks
+     *
+     * @return string 
+     */
+    public function getRemarks()
+    {
+        return $this->remarks;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Area
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     * @return Area
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime 
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return Area
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Set isPublic
+     *
+     * @param boolean $isPublic
+     * @return Area
+     */
+    public function setIsPublic($isPublic)
+    {
+        $this->isPublic = $isPublic;
+    
+        return $this;
+    }
+
+    /**
+     * Get isPublic
+     *
+     * @return boolean 
+     */
+    public function getIsPublic()
+    {
+        return $this->isPublic;
+    }
+
+    /**
+     * Set isDelete
+     *
+     * @param boolean $isDelete
+     * @return Area
+     */
+    public function setIsDelete($isDelete)
+    {
+        $this->isDelete = $isDelete;
+    
+        return $this;
+    }
+
+    /**
+     * Get isDelete
+     *
+     * @return boolean 
+     */
+    public function getIsDelete()
+    {
+        return $this->isDelete;
+    }
+
+    /**
+     * Set fromrefbucket
+     *
+     * @param integer $fromrefbucket
+     * @return Area
+     */
+    public function setFromrefbucket($fromrefbucket)
+    {
+        $this->fromrefbucket = $fromrefbucket;
+    
+        return $this;
+    }
+
+    /**
+     * Get fromrefbucket
+     *
+     * @return integer 
+     */
+    public function getFromrefbucket()
+    {
+        return $this->fromrefbucket;
+    }
+
+    /**
+     * Set torefbucket
+     *
+     * @param integer $torefbucket
+     * @return Area
+     */
+    public function setTorefbucket($torefbucket)
+    {
+        $this->torefbucket = $torefbucket;
+    
+        return $this;
+    }
+
+    /**
+     * Get torefbucket
+     *
+     * @return integer 
+     */
+    public function getTorefbucket()
+    {
+        return $this->torefbucket;
+    }
+
+    /**
+     * Set fromrefus
+     *
+     * @param integer $fromrefus
+     * @return Area
+     */
+    public function setFromrefus($fromrefus)
+    {
+        $this->fromrefus = $fromrefus;
+    
+        return $this;
+    }
+
+    /**
+     * Get fromrefus
+     *
+     * @return integer 
+     */
+    public function getFromrefus()
+    {
+        return $this->fromrefus;
+    }
+
+    /**
+     * Set torefus
+     *
+     * @param integer $torefus
+     * @return Area
+     */
+    public function setTorefus($torefus)
+    {
+        $this->torefus = $torefus;
+    
+        return $this;
+    }
+
+    /**
+     * Get torefus
+     *
+     * @return integer 
+     */
+    public function getTorefus()
+    {
+        return $this->torefus;
+    }
+
+    /**
+     * Set type
+     *
+     * @param \Kdig\ArchaeologicalBundle\Entity\VocAreaType $type
+     * @return Area
+     */
+    public function setType(\Kdig\ArchaeologicalBundle\Entity\VocAreaType $type = null)
+    {
+        $this->type = $type;
+    
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return \Kdig\ArchaeologicalBundle\Entity\VocAreaType 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Add uss
+     *
+     * @param \Kdig\ArchaeologicalBundle\Entity\Us $uss
+     * @return Area
+     */
+    public function addUs(\Kdig\ArchaeologicalBundle\Entity\Us $uss)
+    {
+        $this->uss[] = $uss;
+    
+        return $this;
+    }
+
+    /**
+     * Remove uss
+     *
+     * @param \Kdig\ArchaeologicalBundle\Entity\Us $uss
+     */
+    public function removeUs(\Kdig\ArchaeologicalBundle\Entity\Us $uss)
+    {
+        $this->uss->removeElement($uss);
+    }
+
+    /**
+     * Get uss
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getUss()
+    {
+        return $this->uss;
+    }
+
+    /**
+     * Add media
+     *
+     * @param \Kdig\MediaBundle\Entty\Media $media
+     * @return Area
+     */
+    public function addMedia(\Kdig\MediaBundle\Entty\Media $media)
+    {
+        $this->media[] = $media;
+    
+        return $this;
+    }
+
+    /**
+     * Remove media
+     *
+     * @param \Kdig\MediaBundle\Entty\Media $media
+     */
+    public function removeMedia(\Kdig\MediaBundle\Entty\Media $media)
+    {
+        $this->media->removeElement($media);
+    }
+
+    /**
+     * Get media
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getMedia()
+    {
+        return $this->media;
+    }
+
+    /**
+     * Add groups
+     *
+     * @param \Kdig\UserBundle\Entity\Group $groups
+     * @return Area
+     */
+    public function addGroup(\Kdig\UserBundle\Entity\Group $groups)
+    {
+        $this->groups[] = $groups;
+    
+        return $this;
+    }
+
+    /**
+     * Remove groups
+     *
+     * @param \Kdig\UserBundle\Entity\Group $groups
+     */
+    public function removeGroup(\Kdig\UserBundle\Entity\Group $groups)
+    {
+        $this->groups->removeElement($groups);
+    }
+
+    /**
+     * Get groups
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getGroups()
+    {
+        return $this->groups;
+    }
+
+    /**
+     * Add userprofilearea
+     *
+     * @param \Kdig\UserBundle\Entity\User $userprofilearea
+     * @return Area
+     */
+    public function addUserprofilearea(\Kdig\UserBundle\Entity\User $userprofilearea)
+    {
+        $this->userprofilearea[] = $userprofilearea;
+    
+        return $this;
+    }
+
+    /**
+     * Remove userprofilearea
+     *
+     * @param \Kdig\UserBundle\Entity\User $userprofilearea
+     */
+    public function removeUserprofilearea(\Kdig\UserBundle\Entity\User $userprofilearea)
+    {
+        $this->userprofilearea->removeElement($userprofilearea);
+    }
+
+    /**
+     * Get userprofilearea
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getUserprofilearea()
+    {
+        return $this->userprofilearea;
+    }
 }
