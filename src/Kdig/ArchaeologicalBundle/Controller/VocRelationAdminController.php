b3aaab009dc0b82a6751a14797d4cd5e4a22c974
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-14 15:19:18 +0200

    sonata admin configuration

---
+++ src/Kdig/ArchaeologicalBundle/Controller/VocRelationAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kdig\ArchaeologicalBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class VocRelationAdminController extends Controller
+{
+
+}
