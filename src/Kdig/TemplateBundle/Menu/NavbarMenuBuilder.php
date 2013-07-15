a913e1e1b50a613a2852e9cb84f80b6b6e523f92
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-15 19:03:46 +0200

    added fixture

--- src/Kdig/TemplateBundle/Menu/NavbarMenuBuilder.php
+++ src/Kdig/TemplateBundle/Menu/NavbarMenuBuilder.php
@@ -64,29 +64,115 @@ class NavbarMenuBuilder extends AbstractNavbarMenuBuilder
         $smp = $menu->addChild('Sample', array('route' => 'default_index'));
         $this->addIcon($smp, array('icon' => 'user', 'inverted'=>true, 'append'=>false ));
         
-        $dropdown = $this->createDropdownMenuItem($menu, "Voc", true, array('caret' => true));
-        $dropdown->setChildrenAttribute('class', 'leftMenu dropdown-menu');
-//        $dropdown->setChildrenAttribute('class', 'nav pull-right');
-        $dropdown->addChild('Prova', array('route' => 'fos_user_security_logout'));
-        $dropdown->addChild('Prova2', array('route' => 'fos_user_security_logout'));
         
-        $dropdown = $this->createDropdownMenuItem($menu, "Voc2", true, array('caret' => true));
-        $dropdown->setChildrenAttribute('class', 'leftMenu dropdown-menu');
-//        $dropdown->setChildrenAttribute('class', 'nav pull-right');
-        $dropdown->addChild('Prova3', array('route' => 'fos_user_security_logout'));
-        $dropdown->addChild('Prova4', array('route' => 'fos_user_security_logout'));
-        // object
-        // voc obj
-        // 
-        // pottery
-        // voc pottery
-        // 
-        // sample
-        // voc sample
-        // 
-//        $dropdown = $this->createDropdownMenuItem($menu, "Mehr");
-//        $dropdown->addChild('Captain Ränge', array('route' => 'revorix_ranks'));
-//        $dropdown->addChild('Schiffs-XP', array('route' => 'revorix_xptool'));
+        if ($securityContext->hasRole('ROLE_ARCHEOLOGO')) {
+            $su = $this->createDropdownMenuItem($menu, "SU", true, array('caret' => true));
+            $su->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $bucket = $this->createDropdownMenuItem($menu, "Bucket", true, array('caret' => true));
+            $bucket->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $obj = $this->createDropdownMenuItem($menu, "Object", true, array('caret' => true));
+            $obj->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $pot = $this->createDropdownMenuItem($menu, "Pottery", true, array('caret' => true));
+            $pot->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $smp = $this->createDropdownMenuItem($menu, "Sample", true, array('caret' => true));
+            $smp->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $media = $this->createDropdownMenuItem($menu, "Media", true, array('caret' => true));
+            $media->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            
+            $su->addChild('New', array('route' => 'us_new'));
+            $su->addChild('List', array('route' => 'us'));
+            $bucket->addChild('New', array('route' => 'bucket_new'));
+            $bucket->addChild('List', array('route' => 'bucket'));
+            $obj->addChild('New', array('route' => 'preobject_new'));
+            $obj->addChild('List', array('route' => 'preobject'));
+            $pot->addChild('New', array('route' => 'prepottery_new'));
+            $pot->addChild('List', array('route' => 'prepottery'));
+            $smp->addChild('New', array('route' => 'presample_new'));
+            $smp->addChild('List', array('route' => 'presample'));
+            $media->addChild('New', array('route' => 'default_index'));
+            $media->addChild('List', array('route' => 'default_index'));
+        
+        } elseif ($securityContext->hasRole('ROLE_CERAMICA')) {
+            $su = $this->createDropdownMenuItem($menu, "SU", true, array('caret' => true));
+            $su->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $bucket = $this->createDropdownMenuItem($menu, "Bucket", true, array('caret' => true));
+            $bucket->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $obj = $this->createDropdownMenuItem($menu, "Object", true, array('caret' => true));
+            $obj->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $pot = $this->createDropdownMenuItem($menu, "Pottery", true, array('caret' => true));
+            $pot->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $smp = $this->createDropdownMenuItem($menu, "Sample", true, array('caret' => true));
+            $smp->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $media = $this->createDropdownMenuItem($menu, "Media", true, array('caret' => true));
+            $media->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            
+            $su->addChild('New', array('route' => 'us_new'));
+            $su->addChild('List', array('route' => 'us'));
+            $bucket->addChild('New', array('route' => 'bucket_new'));
+            $bucket->addChild('List', array('route' => 'bucket'));
+            $obj->addChild('New', array('route' => 'preobject_new'));
+            $obj->addChild('List', array('route' => 'preobject'));
+            $pot->addChild('New', array('route' => 'prepottery_new'));
+            $pot->addChild('List', array('route' => 'prepottery'));
+            $smp->addChild('New', array('route' => 'presample_new'));
+            $smp->addChild('List', array('route' => 'presample'));
+            $media->addChild('New', array('route' => 'default_index'));
+            $media->addChild('List', array('route' => 'default_index'));
+            
+        } elseif ($securityContext->hasRole('ROLE_OGGETTI')) {
+            $su = $this->createDropdownMenuItem($menu, "SU", true, array('caret' => true));
+            $su->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $bucket = $this->createDropdownMenuItem($menu, "Bucket", true, array('caret' => true));
+            $bucket->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $obj = $this->createDropdownMenuItem($menu, "Object", true, array('caret' => true));
+            $obj->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $pot = $this->createDropdownMenuItem($menu, "Pottery", true, array('caret' => true));
+            $pot->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $smp = $this->createDropdownMenuItem($menu, "Sample", true, array('caret' => true));
+            $smp->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $media = $this->createDropdownMenuItem($menu, "Media", true, array('caret' => true));
+            $media->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            
+            $su->addChild('New', array('route' => 'us_new'));
+            $su->addChild('List', array('route' => 'us'));
+            $bucket->addChild('New', array('route' => 'bucket_new'));
+            $bucket->addChild('List', array('route' => 'bucket'));
+            $obj->addChild('New', array('route' => 'preobject_new'));
+            $obj->addChild('List', array('route' => 'preobject'));
+            $pot->addChild('New', array('route' => 'prepottery_new'));
+            $pot->addChild('List', array('route' => 'prepottery'));
+            $smp->addChild('New', array('route' => 'presample_new'));
+            $smp->addChild('List', array('route' => 'presample'));
+            $media->addChild('New', array('route' => 'default_index'));
+            $media->addChild('List', array('route' => 'default_index'));
+            
+        } elseif ($securityContext->hasRole('ROLE_SAMPLE')) {
+            $su = $this->createDropdownMenuItem($menu, "SU", true, array('caret' => true));
+            $su->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $bucket = $this->createDropdownMenuItem($menu, "Bucket", true, array('caret' => true));
+            $bucket->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $obj = $this->createDropdownMenuItem($menu, "Object", true, array('caret' => true));
+            $obj->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $pot = $this->createDropdownMenuItem($menu, "Pottery", true, array('caret' => true));
+            $pot->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $smp = $this->createDropdownMenuItem($menu, "Sample", true, array('caret' => true));
+            $smp->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            $media = $this->createDropdownMenuItem($menu, "Media", true, array('caret' => true));
+            $media->setChildrenAttribute('class', 'leftMenu dropdown-menu');
+            
+            $su->addChild('New', array('route' => 'us_new'));
+            $su->addChild('List', array('route' => 'us'));
+            $bucket->addChild('New', array('route' => 'bucket_new'));
+            $bucket->addChild('List', array('route' => 'bucket'));
+            $obj->addChild('New', array('route' => 'preobject_new'));
+            $obj->addChild('List', array('route' => 'preobject'));
+            $pot->addChild('New', array('route' => 'prepottery_new'));
+            $pot->addChild('List', array('route' => 'prepottery'));
+            $smp->addChild('New', array('route' => 'presample_new'));
+            $smp->addChild('List', array('route' => 'presample'));
+            $media->addChild('New', array('route' => 'default_index'));
+            $media->addChild('List', array('route' => 'default_index'));
+        }
 
         return $menu;
     }
