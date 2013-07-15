9e45eefc0346efdf9edb2cb815a3610f90b82b5d
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-15 11:10:10 +0200

    added fixture

--- src/Kdig/ArchaeologicalBundle/DataFixtures/ORM/LoadSiteData.php
+++ src/Kdig/ArchaeologicalBundle/DataFixtures/ORM/LoadSiteData.php
@@ -2,7 +2,8 @@
 
 namespace Kdig\ArchaeologicalBundle\DataFixtures\ORM;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Kdig\ArchaeologicalBundle\Entity\Site;
 
