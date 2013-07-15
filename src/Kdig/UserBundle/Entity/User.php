6e2642ec45dfdce182e4b7e590169f6edf180cf8
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-15 10:12:12 +0200

    update user entity

--- src/Kdig/UserBundle/Entity/User.php
+++ src/Kdig/UserBundle/Entity/User.php
@@ -32,33 +32,16 @@ use Gedmo\Mapping\Annotation as Gedmo;
  */
 class User extends BaseUser
 {
-	const ROLE_DEFAULT = 'ROLE_ALLOWED_TO_SWITCH';
-	
     /**
-    * @var bigint $id
-    * 
-    * @ORM\Column(name="id", type="bigint", nullable=false)
-    * @ORM\Id
-    * @ORM\GeneratedValue(strategy="IDENTITY")
-    */
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     \*/
     protected $id;
-
-    /**
-     * @var string $name
-     * @ORM\Column(name="name", type="string", nullable = true)
-     */
-    protected $name;
-
-    /**
-    * @var string $nickname
-    * @ORM\Column(name="nickname", type="string", nullable = true)
-    */
-    protected $nickname;	
-	
-
+    
     /**
      * @ORM\ManyToMany(targetEntity="Kdig\UserBundle\Entity\Group")
-     * @ORM\JoinTable(name="fos_user_group",
+     * @ORM\JoinTable(name="fos_user_user_group",
      *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
      * )
@@ -76,41 +59,7 @@ class User extends BaseUser
      * @ORM\JoinColumn(nullable=true, name="area_id", referencedColumnName="id")
      */
     protected $slectedarea;
-     
     
-    /**
-     * @var array
-     * @ORM\Column(type="array")
-     */
-    protected $permissions;    
-    
-    /**
-     * @var \DateTime
-     */
-    public $expiresAt;
-    
-    /**
-     * @var \DateTime
-     */
-    public $credentialsExpireAt;    
-
-    public function __construct()
-    {
-    	parent::__construct();
-    	$this->groups		= new \Doctrine\Common\Collections\ArrayCollection();
-    }    
-    
-    /**
-     *
-     * This method is used when you want to convert to string an object of
-     * this Entity
-     * ex: $value = (string)$entity;
-     *
-     */
-    public function __toString() {
-    	return (string) $this->username;
-    }    
-
     /**
      * Get id
      *
@@ -122,163 +71,42 @@ class User extends BaseUser
     }
 
     /**
-     * Add groups
+     * Set slectedgroup
      *
-     * @param \Kdig\UserBundle\Entity\Group $groups
+     * @param Kdig\UserBundle\Entity\Group $slectedgroup
      */
-    public function addGroupUser(\Kdig\UserBundle\Entity\Group $groups)
+    public function setSlectedgroup(\Kdig\UserBundle\Entity\Group $slectedgroup)
     {
-        $this->groups[] = $groups;
-    }
-	
-    /**
-     * Get groups
-     *
-     * @return \Doctrine\Common\Collections\ArrayCollection
-     */
-    public function getGroupsUser()
-    {
-        return $this->groups;
-    }
-    
-    /**
-     * Set permissions
-     *
-     * @param array $permissions
-     */
-    public function setPermissions(array $permissions)
-    {
-    	$this->permissions = array();
-    
-    	foreach ($permissions as $permission) {
-    		$this->addPermission($permission);
-    	}
-    }
-    
-    /**
-     * Get permissions
-     *
-     * @return array
-     */
-    public function getPermissions()
-    {
-    	$permissions = $this->permissions;
-    
-    	// we need to make sure to have at least one role
-    	$permissions[] = PermissionRepository::ShowDefaultPermission();
-    
-    	return array_unique($permissions);
-    }   
-
-    /**
-     * Adds a permission to the user.
-     *
-     * @param string $permission
-     */
-    public function addPermission($permission)
-    {
-    	$permission = strtoupper($permission);
-    
-    	if (!in_array($permission, $this->permissions, true)) {
-    		$this->permissions[] = $permission;
-    	}
-    }  
-
-    /**
-     * Adds a role to the user.
-     *
-     * @param string $role
-     */
-    public function addRole($role)
-    {
-    	$role = strtoupper($role);
-    	if ($role === static::ROLE_DEFAULT) {
-    		return;
-    	}
-    
-    	if (!in_array($role, $this->roles, true)) {
-    		$this->roles[] = $role;
-    	}
+        $this->slectedgroup = $slectedgroup;
     }
 
     /**
-     * Returns the user roles
+     * Get slectedgroup
      *
-     * Implements SecurityUserInterface
-     *
-     * @return array The roles
+     * @return Kdig\UserBundle\Entity\Group 
      */
-    public function getRoles()
+    public function getSlectedgroup()
     {
-    	$roles = $this->roles;
-    
-    	foreach ($this->getGroups() as $group) {
-    		$roles = array_merge($roles, $group->getRoles());
-    	}
-    	
-    	// we need to make sure to have at least one role
-    	$roles[] = static::ROLE_DEFAULT;
-    	
-    	return array_unique($roles);
+        return $this->slectedgroup;
     }
-    
+
     /**
-     * Set name
+     * Set slectedarea
      *
-     * @param text $name
+     * @param Kdig\ArchaeologicalBundle\Entity\Area $slectedarea
      */
-    public function setName($name)
+    public function setSlectedarea(\Kdig\ArchaeologicalBundle\Entity\Area $slectedarea)
     {
-    	$this->name = $name;
+        $this->slectedarea = $slectedarea;
     }
-    
+
     /**
-     * Get name
+     * Get slectedarea
      *
-     * @return text
+     * @return Kdig\ArchaeologicalBundle\Entity\Area 
      */
-    public function getName()
+    public function getSlectedarea()
     {
-    	return $this->name;
+        return $this->slectedarea;
     }
-    
-    /**
-     * Set nickname
-     *
-     * @param text $nickname
-     */
-    public function setNickname($nickname)
-    {
-    	$this->nickname = $nickname;
-    }
-    
-    /**
-     * Get nickname
-     *
-     * @return text
-     */
-    public function getNickname()
-    {
-    	return $this->nickname;
-    }
-    
-    /**
-     * get Updated
-     *
-     * @param datetime $updated
-     */
-    public function getUpdated()
-    {
-        return $this->updated;
-    }
-    
-    /**
-     * Get created_at
-     *
-     * @return datetime
-     */
-    public function getCreatedAt()
-    {
-    	return $this->created_at;
-    }    
 }
