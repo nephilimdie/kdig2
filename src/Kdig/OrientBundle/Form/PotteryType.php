1e9235a2cc8c7802b4d6df59c8961391ca51c0f5
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-16 00:55:08 +0200

    update

--- src/Kdig/OrientBundle/Form/PotteryType.php
+++ src/Kdig/OrientBundle/Form/PotteryType.php
@@ -6,6 +6,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+use Kdig\ArchaeologicalBundle\Form\PrepotteryType;
+
 class PotteryType extends AbstractType
 {
     protected $bucketid;
