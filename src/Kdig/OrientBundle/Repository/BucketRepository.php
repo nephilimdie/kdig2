8b83f7da1e639251ea7eaf16edcc43147f907777
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-15 19:23:34 +0200

    added fixture

---
+++ src/Kdig/OrientBundle/Repository/BucketRepository.php
@@ -0,0 +1,77 @@
+<?php
+namespace Kdig\OrientBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class BucketRepository extends EntityRepository
+{
+    public function getmygroupelement($user)
+    {
+        $group = $user->getSlectedgroup();
+        //$areaid = getSlectedarea();
+        foreach ($group->getAreas() as $area)
+            $areas[]=$area->getId(); 
+        
+        $ids = array();
+        $expr = new \Doctrine\ORM\Query\Expr();
+        
+        $result = $this->createQueryBuilder('b')
+            ->select('b', 'u')
+            ->from('KdigArchaeologicalBundle:Us', 'u')
+            ->innerJoin('u.area', 'aid','WITH', $expr->in('u.area', $areas))
+//            ->add('where', $result->expr()->in('u.area', $areas))
+            ->where('u.id = b.us')
+            ->getQuery()->getResult();
+        
+        foreach ($result as $id)
+            $ids[]=$id->getId(); 
+        
+        return $ids;
+    }
+    public function freeName($sigla, $user) 
+    {
+        $area = $user->getSlectedarea();
+        $Frombucket = $area->getFromrefbucket();
+        $siteid = 1;//$sigla->getId();
+        
+        $Tobucket = $area->getTorefbucket();
+        $numdef = $Frombucket;  
+        
+        $presigla = $sigla->getSigla().'P.';
+
+        $newvec = array();
+        
+        $result = $this->createQueryBuilder('b')
+            ->select('b', 'u')
+            ->from('KdigArchaeologicalBundle:Us', 'u')
+            ->setParameter('area', $area)
+                // da cambiare con quello attivo e non con 1
+            ->setParameter('siteid', $siteid)
+            ->innerJoin('u.area', 'aid','WITH', 'aid = :area' )
+            ->where('u.id = b.us')
+            ->andWhere('u.site = :siteid')
+            ->orderBy('b.name', 'ASC')
+            ->getQuery()->getResult();
+        
+            foreach ($result as $str){
+                $stringa = str_replace($presigla,'', $str->getName());
+                $newvec[] = (int)$stringa;
+            }
+
+            $count = count($newvec);
+            $numC = (int)$Frombucket;
+            
+            for ( $i = 0; $i < $count; $i++ )
+            {
+                if(!in_array($numC, $newvec)) {
+                        $let = $numC;
+                return ($presigla.str_pad($let, 4 , "0000", STR_PAD_LEFT));
+                }
+                $numC++;
+            }
+
+        return $stringa = $presigla.str_pad($numC, 4 , "0000", STR_PAD_LEFT);
+    }
+    
+}
