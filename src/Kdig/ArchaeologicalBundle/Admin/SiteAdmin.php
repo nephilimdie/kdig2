ff332db2f3cbb4d61e64e53c7acb7eb63f6579a8
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-15 09:44:07 +0200

    add requirede false for media

--- src/Kdig/ArchaeologicalBundle/Admin/SiteAdmin.php
+++ src/Kdig/ArchaeologicalBundle/Admin/SiteAdmin.php
@@ -87,7 +87,7 @@ class SiteAdmin extends Admin
             ->add('remarks')
             ->add('campagna')
             ->add('sigla')
-            ->add('media')
+            ->add('media', null, array('required' => false))
             ->add('isPublic')
         ;
     }
