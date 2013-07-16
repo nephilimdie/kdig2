1a968459ae5018cc395b4744d486037719460edd
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-17 01:25:30 +0200

    update

--- src/Kdig/TemplateBundle/Menu/NavbarMenuBuilder.php
+++ src/Kdig/TemplateBundle/Menu/NavbarMenuBuilder.php
@@ -128,25 +128,11 @@ class NavbarMenuBuilder extends AbstractNavbarMenuBuilder
             $su->setChildrenAttribute('class', 'leftMenu dropdown-menu');
 //            $bucket = $this->createDropdownMenuItem($menu, "Bucket", true, array('caret' => true));
 //            $bucket->setChildrenAttribute('class', 'leftMenu dropdown-menu');
-            $obj = $this->createDropdownMenuItem($menu, "Object", true, array('caret' => true));
-            $obj->setChildrenAttribute('class', 'leftMenu dropdown-menu');
-            $pot = $this->createDropdownMenuItem($menu, "Pottery", true, array('caret' => true));
-            $pot->setChildrenAttribute('class', 'leftMenu dropdown-menu');
-            $smp = $this->createDropdownMenuItem($menu, "Sample", true, array('caret' => true));
-            $smp->setChildrenAttribute('class', 'leftMenu dropdown-menu');
-            $media = $this->createDropdownMenuItem($menu, "Media", true, array('caret' => true));
-            $media->setChildrenAttribute('class', 'leftMenu dropdown-menu');
             
-            $su->addChild('New', array('route' => 'us_new'));
-            $su->addChild('List', array('route' => 'us'));
 //            $bucket->addChild('New', array('route' => 'bucket_new'));
 //            $bucket->addChild('List', array('route' => 'bucket'));
             $obj->addChild('New', array('route' => 'preobject_new'));
             $obj->addChild('List', array('route' => 'preobject'));
-            $pot->addChild('New', array('route' => 'prepottery_new'));
-            $pot->addChild('List', array('route' => 'prepottery'));
-            $smp->addChild('New', array('route' => 'presample_new'));
-            $smp->addChild('List', array('route' => 'presample'));
             $media->addChild('New', array('route' => 'default_index'));
             $media->addChild('List', array('route' => 'default_index'));
             
