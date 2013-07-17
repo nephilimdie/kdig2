93a2e441b1b2e98f623aa8ff8c78cedff5e5d078
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-17 13:26:38 +0200

    added easy CSV

--- app/autoload.php
+++ app/autoload.php
@@ -10,6 +10,7 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 
 //$loader-> add(array('Sonata' => __DIR__));
 $loader->add('PHPExcel',__DIR__ . '/../vendor/phpexcel/Classes');
+$loader->add('EasyCSV',__DIR__ . '/../vendor/easy-csv/lib');
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
