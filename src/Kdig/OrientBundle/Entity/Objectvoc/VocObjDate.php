56fca415b7903b211386d50fafe66a1cb8228f3c
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-17 15:27:14 +0200

    object repository

--- src/Kdig/OrientBundle/Entity/Objectvoc/VocObjDate.php
+++ src/Kdig/OrientBundle/Entity/Objectvoc/VocObjDate.php
@@ -19,6 +19,7 @@ use APY\DataGridBundle\Grid\Mapping as GRID;
  * @ORM\Entity
  * @ORM\Table(name="voc_obj_date", schema="public")
  * @Gedmo\Loggable
+ * @ORM\Entity(repositoryClass="Kdig\OrientBundle\Repository\VocObjDateRepository")
  */
 class VocObjDate {
     
