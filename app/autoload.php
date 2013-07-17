23539d3557c78f22faadab221a62762dc06aa044
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-17 02:24:00 +0200

    added php excel

--- app/autoload.php
+++ app/autoload.php
@@ -9,10 +9,7 @@ use Composer\Autoload\ClassLoader;
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 //$loader-> add(array('Sonata' => __DIR__));
-$loader->registerPrefixes(array(
-    // Swift, Twig etc.
-    'PHPExcel' => __DIR__ . '/../vendor/phpexcel/Classes'
-));
+$loader->add('PHPExcel',__DIR__ . '/../vendor/phpexcel/Classes');
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
