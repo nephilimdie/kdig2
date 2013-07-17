1bba5d998dfe6e8e6a11ac36c071415053ea8b49
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-17 15:10:55 +0200

    object repository

---
+++ src/Kdig/OrientBundle/Repository/VocObjPreservationRepository.php
@@ -0,0 +1,34 @@
+<?php
+namespace Kdig\OrientBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class VocObjPreservationRepository extends EntityRepository
+{
+    private function isUnusedMaterial($name) {        
+        $result = $this->createQueryBuilder('u')
+            ->select('u')
+            ->from('KdigOrientBundle:Objectvoc\VocObjPreservation', 'u')
+            ->where('u.name = :name')
+            ->setParameter('name', $name)
+            ->createQuery();
+
+        $res = $result->getResult();
+        return empty($res);
+    }
+    public function checkElement($name){
+        if($this->isUnusedMaterial($name)) {
+            //createnew
+            $em = $this->getDoctrine()->getEntityManager();
+            $entity = new \Kdig\OrientBundle\Entity\Objectvoc\VocObjPreservation();
+            $entity -> setName($name);
+            $em->persist($entity);
+            $em->flush();
+            return $entity;
+        } else {
+            return $entity = $this->findOneBy(array('name'=>$name));
+        }
+    }
+    
+}
