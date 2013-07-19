1c73052e3ad585db48bd3537460258163518d760
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-19 14:52:57 +0200

    string aggragation added

--- src/Kdig/ArchaeologicalBundle/Controller/DefaultController.php
+++ src/Kdig/ArchaeologicalBundle/Controller/DefaultController.php
@@ -26,7 +26,31 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array();
+        // Chart
+        $series = array(
+            array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
+        );
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');
+        $ob->title->text('Browser market shares at a specific website in 2010');
+        $ob->plotOptions->pie(array(
+            'allowPointSelect'  => true,
+            'cursor'    => 'pointer',
+            'dataLabels'    => array('enabled' => false),
+            'showInLegend'  => true
+        ));
+        $data = array(
+            array('Firefox', 45.0),
+            array('IE', 26.8),
+            array('Chrome', 12.8),
+            array('Safari', 8.5),
+            array('Opera', 6.2),
+            array('Others', 0.7),
+        );
+        $ob->series(array(array('type' => 'pie','name' => 'Browser share', 'data' => $data)));
+        
+        return array('chart' => $ob);
     }
     
     /**
