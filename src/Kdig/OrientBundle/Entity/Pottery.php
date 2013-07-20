30d120845450cff9e6bfab2a76ea1c8a38fa1d4b
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-20 15:20:31 +0200

    left hightchart

--- src/Kdig/OrientBundle/Entity/Pottery.php
+++ src/Kdig/OrientBundle/Entity/Pottery.php
@@ -246,7 +246,7 @@ class Pottery {
       
     /**
      * @ORM\OneToMany(targetEntity="Kdig\OrientBundle\Entity\Potteryvoc\VocPotterySurfacetratin", mappedBy="pottery", orphanRemoval=true) 
-     * @GRID\Column(field="surfacetratin", title="Surface Treatment In")
+     * @GRID\Column(field="surfacetratin.__tostring", title="Surface Treatment In")
      * @GRID\Column(field="surfacetratin.color.name", title="Color")
      */
     private $surfacetratin;
