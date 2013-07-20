785f676dbc3e0535c146881e4da334abd3e73cfd
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-20 18:42:28 +0200

    correct string aggregation for postgres

--- src/Kdig/ArchaeologicalBundle/DQL/StringAgg.php
+++ src/Kdig/ArchaeologicalBundle/DQL/StringAgg.php
@@ -17,23 +17,38 @@ use Doctrine\ORM\Query\Lexer;
 class StringAgg extends FunctionNode
 {
     public $expression = null;
+    public $secondExpression = null;
     public $delimiter = null;
 
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
+        // use second parameter if parsed
+        if (null !== $this->secondExpression){
+            return 'string_agg(' 
+                . $this->expression->dispatch($sqlWalker)
+                . ', '
+                . $this->secondExpression->dispatch($sqlWalker)
+                . ')';
+        }
+
         return 'string_agg(' .
-            $this->expression->dispatch($sqlWalker) . ', ' .
-            $this->delimiter->dispatch($sqlWalker) .
-        ')';
+            $this->expression->dispatch($sqlWalker)
+            .')';
     }
 
     public function parse(\Doctrine\ORM\Query\Parser $parser)
     {
+        $lexer = $parser->getLexer();
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
         $this->expression = $parser->SingleValuedPathExpression();
-        if($parser->match(Lexer::T_COMMA))
-            $this->delimiter = $parser->ArithmeticExpression();
+        
+        // parse second parameter if available
+        if(Lexer::T_COMMA === $lexer->lookahead['type']){
+            $parser->match(Lexer::T_COMMA);
+            $this->secondExpression = $parser->ArithmeticPrimary();
+        }
+        
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 }
