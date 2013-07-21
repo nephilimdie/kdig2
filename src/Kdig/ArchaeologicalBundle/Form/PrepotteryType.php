58e5a89843bd53521035921f5e226632d7dfaed7
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-22 00:52:58 +0200

    post update post persist to fit tcode

--- src/Kdig/ArchaeologicalBundle/Form/PrepotteryType.php
+++ src/Kdig/ArchaeologicalBundle/Form/PrepotteryType.php
@@ -8,15 +8,30 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class PrepotteryType extends AbstractType
 {
+    protected $bucketid;
+    
+    public function __construct($bucketid = null)
+    {
+        $this->bucketid = $bucketid;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $bucketid = $this->bucketid;
+        
         $builder
+            ->add('bucket', 'entity', array(
+                'class' => 'KdigArchaelogicalBundle:Bucket',
+                'query_builder' => function($repository) use ($bucketid) {
+                    $expr = new \Doctrine\ORM\Query\Expr();
+                    $idiota = $repository->createQueryBuilder('p')
+                        ->setParameter('buckets', $bucketid)
+                        ->add('where', ($expr->in('p.id', ':buckets')))
+                        ->orderBy('p.name', 'ASC'); 
+                    return $idiota;
+                }
+            ))
             ->add('name')
             ->add('remarks')
-            ->add('bucket', 'genemu_jqueryselect2_entity', array(
-                'class' => 'Kdig\OrientBundle\Entity\Bucket',
-                'property' => 'name'
-            ))
         ;
     }
 
