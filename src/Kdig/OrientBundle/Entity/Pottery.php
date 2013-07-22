13a7e8b2cf0a59a3388a661875b64e662b40344d
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-22 21:52:30 +0200

    pottery

--- src/Kdig/OrientBundle/Entity/Pottery.php
+++ src/Kdig/OrientBundle/Entity/Pottery.php
@@ -32,7 +32,7 @@ class Pottery {
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
      * @Gedmo\Versioned
      * @GRID\Column(title="ID", size="50",visible=false)
      */
