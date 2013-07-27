85db31eb961d0844f1e563756970cfd839161d44
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-27 21:19:18 +0200

    fixed groups

--- src/Kdig/UserBundle/Controller/DefaultController.php
+++ src/Kdig/UserBundle/Controller/DefaultController.php
@@ -10,6 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 
 use Kdig\UserBundle\Entity\User as User;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 /**
  * Bucket controller.
@@ -27,11 +28,22 @@ class DefaultController extends Controller
         return false;
     }
     /**
-     * @Route("/", name="user_change_group")
+     * @Route("/group/", name="user_change_group")
      * @Method("POST")
+     * @Secure(roles="IS_AUTHENTICATED_FULLY")
      */
     public function changeGroupAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $user = $this->get('security.context')->getToken()->getUser();
+        $group = $em->getRepository('KdigUserBundle:Group')->find($request->get('group_id'));
+        
+        if (!$group) {
+            throw $this->createNotFoundException('Unable to find group entity.');
+        }
+        $entity->setSlectedgroup($group);
+        $em->persist($entity);
+        $em->flush();
         return false;
     }
 }
