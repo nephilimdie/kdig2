879d77a9543ccd6c09e2d449c6fa7935643161ff
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-27 19:28:09 +0200

    bucket Listener

---
+++ src/Kdig/OrientBundle/Listener/BucketListener.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Kdig\OrientBundle\Listener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Kdig\OrientBundle\Entity\Bucket as Bucket;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class BucketListener 
+{
+    private $container;
+ 
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+ 
+    public function postUpdate(LifecycleEventArgs $args) {
+        $entity = $args->getEntity();
+        $entityManager = $args->getEntityManager();
+        $aclProvider = $this->container->get('problematic.acl_manager');
+        
+        if ($entity instanceof Bucket) {
+            //add ACL information
+            $user = $this->container->get('security.context')->getToken()->getUser();
+            if ($user) {
+                $aclProvider->addObjectPermission($entity, MaskBuilder::MASK_OWNER, $user);
+            }
+        }
+    }
+    
+    public function postPersist(LifecycleEventArgs $args) {
+//        $entity = $args->getEntity();
+//        $entityManager = $args->getEntityManager();
+//        if ($entity instanceof Pottery) {
+//            $tcode = $entity->getClass()->getNumber()
+//                    .$entity->getShape()->getNumber()
+//                    .'-'
+//                    .$entity->getRim()->getNumber()
+//                    .$entity->getNeck()->getNumber()
+//                    .$entity->getWall()->getNumber()
+//                    .$entity->getUpperwall()->getNumber()
+//                    .$entity->getLowerwall()->getNumber()
+//                    .$entity->getBase()->getNumber()
+//                    .$entity->getHandle()->getNumber()
+//                    .$entity->getHandleposition()->getNumber()
+//                    .$entity->getSpout()->getNumber()
+//                    .$entity->getSpoutposition()->getNumber()
+//                    .'-'
+//                    .$entity->getInclusion()->getNumber()
+//                    .$entity->getInclusionsize()->getNumber()
+//                    .$entity->getInclusionfrequency()->getNumber();
+//            $entity->setTcode($tcode);
+//            $entityManager->persist($entity);
+//            $entityManager->flush();
+//        }
+    }
+//    public function prePersist(LifecycleEventArgs $args) {
+//        $entity = $args->getEntity();
+//        $entityManager = $args->getEntityManager();
+//        if ($entity instanceof \Surgeworks\CoreBundle\Entity\ItemsToCollections) {
+//            $item_id = $entity->getItemId();
+//            $item = $entityManager->getRepository('Surgeworks\CoreBundle\Entity\Item')->find($item_id);
+//            $stat_symb = $item->getItemStatus()->getStatusSymbol();
+//            if ($stat_symb != 'st_live') {
+//                $entity->setStatusId($item->getStatusId());
+//            }
+//        }
+//    }
+}
