4e4043af46c81078f60759f49b898ebf66ff80ec
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-27 10:28:41 +0200

    unusedid

---
+++ src/Kdig/ArchaeologicalBundle/Repository/SiteRepository.php
@@ -0,0 +1,18 @@
+<?php
+namespace Kdig\ArchaeologicalBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class SiteRepository extends EntityRepository
+{
+    private function isUnusedName($id) {        
+        $result = $this->createQueryBuilder('u')
+            ->select('u')
+            ->where('u.id = :id')
+            ->setParameter('id', $id)
+            ->getQuery();
+
+        $res = $result->getResult();
+        return empty($res);
+    }
+}
