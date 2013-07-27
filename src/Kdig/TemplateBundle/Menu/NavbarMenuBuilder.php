50ce18d03fdee139ec691459f20156e25773e67e
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-27 20:52:30 +0200

    fixed groups

--- src/Kdig/TemplateBundle/Menu/NavbarMenuBuilder.php
+++ src/Kdig/TemplateBundle/Menu/NavbarMenuBuilder.php
@@ -73,6 +73,7 @@ class NavbarMenuBuilder extends AbstractNavbarMenuBuilder
             $dropdown = $this->createDropdownMenuItem($menu, "Group", true, array('caret' => true));
             foreach ($this->usr->getGroups() as $group)
                 $dropdown->addChild($group->getName(), array('route' => 'user_change_group', 'routeParameters' => array('id' => $group->getId())));
+            
             return $menu;
         }
         return false;
