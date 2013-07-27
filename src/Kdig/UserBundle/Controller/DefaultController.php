0e6647237bcb1cf1412caad54e0b62eb3fc4ce72
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-27 20:28:29 +0200

    fixed groups

--- src/Kdig/UserBundle/Controller/DefaultController.php
+++ src/Kdig/UserBundle/Controller/DefaultController.php
@@ -2,12 +2,32 @@
 
 namespace Kdig\UserBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
+use Kdig\UserBundle\Entity\User as User;
+
+/**
+ * Bucket controller.
+ *
+ * @Route("/user")
+ */
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('KdigUserBundle:Default:index.html.twig', array('name' => $name));
+        return false;
+    }
+    /**
+     * @Route("/{group_id}", name="user_change_group")
+     * @Method("POST")
+     */
+    public function changeGroupAction(Request $request)
+    {
+        return false;
     }
 }
