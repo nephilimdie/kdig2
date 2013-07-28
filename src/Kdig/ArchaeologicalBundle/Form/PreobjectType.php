e031cabcb9e0c3a5bbf628cc8f2b1a25ab284492
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-28 12:42:59 +0200

    update

--- src/Kdig/ArchaeologicalBundle/Form/PreobjectType.php
+++ src/Kdig/ArchaeologicalBundle/Form/PreobjectType.php
@@ -8,11 +8,33 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class PreobjectType extends AbstractType
 {
+    protected $bucketid;
+    protected $inobject;
+    protected $us;
+    
+    public function __construct($bucketid, $usid = null)
+    {
+        $this->bucketid = $bucketid;
+        $this->us = $usid;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $bucketid = $this->bucketid;
+        $usid = $this->us;
+        
         $builder
-            ->add('name', null, array(
-                'attr'=> array('class'=>'span3')
+            ->add('bucket', 'entity', array(
+                'class' => 'KdigArchaelogicalBundle:Bucket',
+                'query_builder' => function($repository) use ($bucketid, $usid) {
+                    $expr = new \Doctrine\ORM\Query\Expr();
+                    $idiota = $repository->createQueryBuilder('p')
+                        ->setParameter('buckets', $bucketid)
+                        ->add('where', ($expr->in('p.id', ':buckets')))
+                            ->orderBy('p.name', 'ASC'); 
+                        if ($usid != null) 
+                            $idiota->setParameter('usid', $usid)->andWhere($expr->in('p.us', ':usid'));
+                    return $idiota;
+                }
             ))
             ->add('bucket', null, array(
                 'attr'=> array('class'=>'span3')
