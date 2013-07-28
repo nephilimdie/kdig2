e346f5f5f44bd8a9b52e06db4b5e679eb6a0049d
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-28 11:13:14 +0200

    bucket form type

--- src/Kdig/OrientBundle/Form/BucketType.php
+++ src/Kdig/OrientBundle/Form/BucketType.php
@@ -8,13 +8,49 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class BucketType extends AbstractType
 {
+    protected $areaid;
+    protected $usid;
+    
+    public function __construct($areaid, $usid = null)
+    {
+        $this->areaid = $areaid;
+        $this->usid = $usid;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $idarea = $this->areaid;
+        $usid = $this->usid;
+        
         $builder
-            ->add('name')
-            ->add('remarks')
-            ->add('imageName')
-            ->add('us')
+            ->add('us', 'entity', array(
+                'class' => 'KdigArchaelogicalBundle:Us',
+                'query_builder' => function($repository) use ($idarea, $usid) {
+                    $expr = new \Doctrine\ORM\Query\Expr();
+                    $idiota = $repository->createQueryBuilder('p')
+                        ->setParameter('idarea', $idarea)
+                        ->add('where', ($expr->in('p.area', ':idarea')))
+//                            ->where('p.area = :idarea')
+                        ->orderBy('p.name', 'ASC'); 
+                    
+                    if($usid) $idiota->setParameter('usid', $usid)->andWhere('p.id = :usid');
+                    
+                    return $idiota;
+                },
+                'required' => true,
+                'label' => 'SU Definition',
+                'help_block'  => 'It defines the SU number'
+            ));
+        $builder
+            ->add('name', null, array(
+                'label' => 'Bucket Definition',
+                'help_block'  => 'It is automatically given by the DB - if does not appear press the reload button'
+            ))
+            ->add('remarks', 'textarea', array(
+                'label' => 'Description',
+                'help_block'  => 'some usefull help message',
+                'required' => false
+            ))
             ->add('media', 'entity', array(
                 'class' => 'KdigMediaBundle:Media',
                 'required' => false,
