4f3f0c7a4fe2ded5f1d9aa4ec4048e2efbb0fbe9
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-28 13:44:53 +0200

    fixed

--- src/Kdig/ArchaeologicalBundle/Form/PrepotteryType.php
+++ src/Kdig/ArchaeologicalBundle/Form/PrepotteryType.php
@@ -9,36 +9,35 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class PrepotteryType extends AbstractType
 {
     protected $bucketid;
+    protected $us;
     
-    public function __construct($bucketid = null)
+    public function __construct($bucketid, $usid = null)
     {
         $this->bucketid = $bucketid;
+        $this->us = $usid;
     }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $bucketid = $this->bucketid;
+        $usid = $this->us;
         
         $builder
-//            ->add('bucket', 'entity', array(
-//                'class' => 'KdigOrientBundle:Bucket',
-//                'query_builder' => function($repository) use ($bucketid) {
-//                    $expr = new \Doctrine\ORM\Query\Expr();
-//                    $idiota = $repository->createQueryBuilder('p')
-//                        ->setParameter('buckets', $bucketid)
-//                        ->add('where', ($expr->in('p.id', ':buckets')))
-//                        ->orderBy('p.name', 'ASC'); 
-//                    return $idiota;
-//                }
-//            ))
-            ->add('bucket', 'genemu_jqueryselect2_entity', array(
-                'class' => 'Kdig\OrientBundle\Entity\Bucket',
-                'property' => 'name',
-                'attr'=> array('class'=>'span3')
+            ->add('bucket', 'entity', array(
+                'class' => 'KdigOrientBundle:Bucket',
+                'query_builder' => function($repository) use ($bucketid, $usid) {
+                    $expr = new \Doctrine\ORM\Query\Expr();
+                    $idiota = $repository->createQueryBuilder('p')
+                        ->setParameter('buckets', $bucketid)
+                        ->add('where', ($expr->in('p.id', ':buckets')))
+                            ->orderBy('p.name', 'ASC'); 
+                        if ($usid != null) 
+                            $idiota->setParameter('usid', $usid)->andWhere($expr->in('p.us', ':usid'));
+                    return $idiota;
+                }
             ))
-            ->add('name', null, array(
+            ->add('name',null,array(
                 'attr'=> array('class'=>'span3')
-            ))
-            ->add('remarks')
+                ))
         ;
     }
 
