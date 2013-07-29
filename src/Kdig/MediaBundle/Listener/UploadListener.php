4992e3abb3f0c1ce878c66fd966bca49d8675a2d
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-30 00:18:26 +0200

    upload

--- src/Kdig/MediaBundle/Listener/UploadListener.php
+++ src/Kdig/MediaBundle/Listener/UploadListener.php
@@ -14,27 +14,48 @@ use Kdig\MediaBundle\Entity\Media;
 
 class UploadListener
 {
-    public function __construct($doctrine)
+    private $container;
+ 
+    public function __construct(ContainerInterface $container)
     {
-        $this->doctrine = $doctrine;
+        $this->container = $container;
     }
 
     public function onUpload(PostPersistEvent $event)
     {
         $request = $event->getRequest();
+        die(var_dump($request));
         $gallery = $request->get('gallery');
-        
+
+            $mediaClass = $mediaAdmin->getClass();
+            /** @var $media \Sonata\MediaBundle\Model\MediaInterface */
+            $media = new $mediaClass();
+
+
         // added with sonata media
+            /** @var $mediaAdmin \Sonata\MediaBundle\Admin\ORM\MediaAdmin */
+            $mediaAdmin = $this->get('sonata.media.admin.media');
+            /** @var $provider \Sonata\MediaBundle\Provider\MediaProviderInterface */
+            $context = $mediaAdmin->getPool()->getDefaultContext();
         $media = new Media();
-        $media->setProviderName('sonata.media.provider.youtube');
-        $media->setBinaryContent('an0dymdMo70');
-        $media->setUser($em->merge($this->getReference('user_one')));
-        //$media->setName('test');
+        $media->setProviderName('sonata.media.provider.file');
+            $media->setEnabled(true);
+            $media->setName($file->getClientOriginalName());
+            $media->setContext($context);
+            $media->setBinaryContent($file);
+            $provider = $this->get($providerName);
+            $path = $provider->generatePublicUrl($media, 'reference');
 
         $this->container->get('sonata.media.pool')->prePersist($media);
         $em->persist($media);
         $em->flush();
         $this->container->get('sonata.media.pool')->postPersist($media);
+        
+        $aclProvider = $this->container->get('problematic.acl_manager');
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if ($user) {
+            $aclProvider->addObjectPermission($media, MaskBuilder::MASK_OWNER, $user);
+        }
 
 
     }
