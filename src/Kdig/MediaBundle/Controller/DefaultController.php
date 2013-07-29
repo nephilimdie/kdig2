54d1a3a9a59e3121ec90a02e79f054fcb4cd9f29
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-07-30 00:18:26 +0200

    upload

--- src/Kdig/MediaBundle/Controller/DefaultController.php
+++ src/Kdig/MediaBundle/Controller/DefaultController.php
@@ -2,12 +2,26 @@
 
 namespace Kdig\MediaBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Bucket controller.
+ *
+ * @Route("/media")
+ */
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/", name="media")
+     * @Template()
+     */
+    public function indexAction()
     {
-        return $this->render('KdigMediaBundle:Default:index.html.twig', array('name' => $name));
+        return array();
     }
 }
