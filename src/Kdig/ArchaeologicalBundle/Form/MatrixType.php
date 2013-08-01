a91a021d8449e3c63df32369f1e5e227decd9024
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-08-01 12:13:24 +0200

    relation between stratigraphic unit

--- src/Kdig/ArchaeologicalBundle/Form/MatrixType.php
+++ src/Kdig/ArchaeologicalBundle/Form/MatrixType.php
@@ -8,17 +8,38 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class MatrixType extends AbstractType
 {
+    protected $areaid;
+    protected $usid;
+    
+    public function __construct($areaid, $usid = null)
+    {
+        $this->areaid = $areaid;
+        $this->usid = $usid;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $idarea = $this->areaid;
+        
         $builder
-            ->add('created')
-            ->add('updated')
-            ->add('isActive')
-            ->add('isPublic')
-            ->add('isDelete')
             ->add('typerelation')
             ->add('fromuss')
-            ->add('touss')
+            ->add('touss', 'entity', array(
+                'class' => 'KdigArchaeologicalBundle:Us',
+                'query_builder' => function($repository) use ($idarea) {
+                    $expr = new \Doctrine\ORM\Query\Expr();
+                    $idiota = $repository->createQueryBuilder('p')
+                        ->setParameter('idarea', $idarea)
+                        ->add('where', ($expr->in('p.area', ':idarea')))
+//                            ->where('p.area = :idarea')
+                        ->orderBy('p.name', 'ASC'); 
+                    
+                    return $idiota;
+                },
+                'required' => true,
+                'label' => 'SU Definition',
+                'help_block'  => 'Select SU'
+            ))
         ;
     }
 
