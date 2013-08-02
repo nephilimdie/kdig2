cdda1cc1b33a8fae123d55a9b85c5f2a4b8b9ea1
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-08-02 12:28:34 +0200

    left getNumber

--- src/Kdig/OrientBundle/Entity/Potteryvoc/VocDecorationOption.php
+++ src/Kdig/OrientBundle/Entity/Potteryvoc/VocDecorationOption.php
@@ -357,6 +357,6 @@ class VocDecorationOption {
     
     public function __tostring() 
     {
-        return (string)$this->getName().'('.$this->getNumber().')';
+        return (string)$this->getName();
     }
 }
