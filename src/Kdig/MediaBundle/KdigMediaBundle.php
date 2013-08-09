480d1ec24e67482757f440c603a24336adaa402b
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-08-09 17:44:31 +0200

    getfile

--- src/Kdig/MediaBundle/KdigMediaBundle.php
+++ src/Kdig/MediaBundle/KdigMediaBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KdigMediaBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'SonataMediaBundle';
+    }
 }
