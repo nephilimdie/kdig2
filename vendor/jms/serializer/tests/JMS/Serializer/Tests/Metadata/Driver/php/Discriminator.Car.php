42a4f49cdc9a560df9d3f56ed8f142137027c11d
stefano.bassetto.nep@gmail.com <stefano.bassetto.nep@gmail.com>
2013-08-09 13:26:25 +0200

    removed

--- vendor/jms/serializer/tests/JMS/Serializer/Tests/Metadata/Driver/php/Discriminator.Car.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use JMS\Serializer\Metadata\ClassMetadata;
-use JMS\Serializer\Metadata\PropertyMetadata;
-
-$metadata = new ClassMetadata('JMS\Serializer\Tests\Fixtures\Discriminator\Car');
-
-return $metadata;
