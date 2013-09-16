13f448c78d2edcd7bf173149a31df012fb7e8cb1
kdig01 <kdig01@kdig01>
2013-09-16 22:08:43 +0300

    fixed grid

--- src/Kdig/OrientBundle/Controller/PotteryController.php
+++ src/Kdig/OrientBundle/Controller/PotteryController.php
@@ -20,7 +20,7 @@ use APY\DataGridBundle\Grid\Action\RowAction;
 use APY\DataGridBundle\Grid\Action\MassAction;
 use APY\DataGridBundle\Grid\Action\DeleteMassAction;
 use APY\DataGridBundle\Grid\Column\ActionsColumn;
-//use APY\DataGridBundle\Grid\Export\PHPExcel2007Export;
+use APY\DataGridBundle\Grid\Export\PHPExcel2007Export;
 
 use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
 
@@ -39,6 +39,7 @@ class PotteryController extends Controller
      *
      * @Route("/", name="pottery")
      * @Breadcrumb("Table", route="pottery")
+     * @Template("KdigTemplateBundle:Default:Grid/grid.html.twig")
      * @Secure(roles="ROLE_ARCHAEOLOGY, ROLE_ADMIN, ROLE_POTTERY, ROLE_SAMPLE, ROLE_OBJECT")
      */
     public function myGridAction()
