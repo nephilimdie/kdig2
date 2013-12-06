60f1b7c673d4f9b7c19d96d8128cd05fb7657625
Stefano Bassetto Mac <stefano.bassetto.nep@gmail.com>
2013-12-06 12:01:27 +0100

    angularjs

---
+++ src/Kdig/TemplateBundle/Resources/public/js/app.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var globalTemplate = '<div compile-html="data"></div>';
+// Declare app level module which depends on filters, and services
+angular.module('commusoft', [
+        'ui.compat', 
+        'kdig.filters', 
+        'kdig.services', 
+        'kdig.directives', 
+        'http-auth-interceptor'
+    ]).
+    constant('prefix', '/app_dev.php').
+    config([
+        '$stateProvider', 
+        '$routeProvider', 
+        '$urlRouterProvider', 
+        '$locationProvider', 
+        '$httpProvider', 
+        'prefix', 
+        function($stateProvider, $routeProvider, $urlRouterProvider, $locationProvider, $httpProvider, prefix) {
+
+            fos.Router.getInstance().setBaseUrl(prefix);
+            $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+            $locationProvider.html5Mode(true).hashPrefix('!');
+
+            $stateProvider  
+            
+                .state('home', {
+                    url: '/',
+                    template: globalTemplate,
+                    templateUrl: function(params) { return Routing.generate('index') },
+                    controller: LogoutCtrl,
+                    resolve: Resolver
+                })
+                .state('us', {
+                    url: '/us',
+                    abstract: true, 
+                    template: globalTemplate,
+                    templateUrl: function(params) { return Routing.generate('us') },
+                    controller: LogoutCtrl,
+                    resolve: Resolver
+                })
+                .state('us', {
+                    url: '/home',
+                    templateUrl: function(params) { return Routing.generate('us') },
+                    controller: LogoutCtrl,
+                    resolve: Resolver
+                })
+                .state('us.show', {
+                    url: '/show/:id',
+                    templateUrl: function(params) { return Routing.generate('us_show', params) },
+                    controller: LogoutCtrl,
+                    resolve: Resolver
+                })
+                .state('us.list', {
+                    url: '/list',
+                    templateUrl: function(params) { return Routing.generate('us_list') },
+                    controller: LogoutCtrl,
+                    resolve: Resolver
+                })
+                .state('us.new', {
+                    url: '/new',
+                    templateUrl: function(params) { return Routing.generate('us_new', params) },
+                    controller: LogoutCtrl,
+                    resolve: Resolver
+                })
+                .state('us.edit', {
+                    url: '/edit/:id',
+                    templateUrl: function(params) { return Routing.generate('us_edit', params) },
+                    controller: LogoutCtrl,
+                    resolve: Resolver
+                })
+                .state('us.delete', {
+                    url: '/delete/:id',
+                    templateUrl: function(params) { return Routing.generate('us_delete', params) },
+                    controller: LogoutCtrl,
+                    resolve: Resolver
+                });
+        }
+    ]).
+    run(function($rootScope, $state) {
+        $rootScope.$on('event:auth-loginRequired', function() {
+            $state.transitionTo('demologin');
+        });
+    });
+    /*
+var kdigApp = angular.module('KdigApp', []).config(function($interpolateProvider){
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+    }
+);
+
+kdigApp.controller('mainCtrl', ['$scope', function ($scope) {
+        
+}]);
+kdigApp.controller('navCtrl', ['$scope', function ($scope) {
+        
+}]);
+kdigApp.controller('userCtrl', ['$scope', function ($scope) {
+        
+}]);
+*/
